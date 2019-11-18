8cd83b03df12175593b092f5907921a848d4c388
rishabh-ion <rsontakke@ionenergy.co>
2019-11-19 00:40:15 +0530

    In progress

--- src/routes/url.js
+++ src/routes/url.js
@@ -4,50 +4,119 @@ var base62 = require("base62/lib/ascii");
 
 const Meta = require("../models/metaModel");
 const Url = require("../models/urlModel");
-// const Meta = require("../models/MetaModel");
+const Visit = require("../models/visitModel");
 
 router.post("/shortenUrl", async (req, res, next) => {
-    var metaData = await Meta.findOne({
-        meta: 1
-    }).select("count -_id").exec();
-    var count = ++metaData.count;
-    new Meta({
-        count: 10000
-    }).save()
-    const shortUrl = await base62.encode(count);
+
     const longUrl = req.body.longUrl;
-    console.log(shortUrl)
 
-    const url = new Url({
-        'longUrl': longUrl,
-        'shortUrl': shortUrl
-    })
+    const isUnique = await Url.findOne({
+        longUrl: longUrl
+    }).select().exec();
 
-    const status = await url.save();
-    if (!status) {
+    console.log(isUnique)
+    if (isUnique) {
         res.status(200).send({
-            isSuccess: false,
+            isSuccess: true,
+            isUnique: false,
+            shortUrl: isUnique.shortUrl
         });
     } else {
-        const result = await Meta.findOneAndUpdate({
-            meta: 1
-        }, {
-            $set: {
-                count: count
-            }
-        })
-        if (!result) {
+        var metaData = await Meta.findOne({
+                meta: 1
+            })
+            .select("count -_id")
+            .exec();
+        var count = ++metaData.count;
+      
+        const shortUrl = await base62.encode(count);
+        const url = new Url({
+            longUrl: longUrl,
+            shortUrl: shortUrl
+        });
+
+        const status = await url.save();
+        if (!status) {
             res.status(200).send({
                 isSuccess: false,
+                isUnique: true
             });
         } else {
-            res.status(201).send({
-                isSuccess: true,
-                shortUrl: shortUrl
+            const result = await Meta.findOneAndUpdate({
+                meta: 1
+            }, {
+                $set: {
+                    count: count
+                }
             });
+            if (!result) {
+                res.status(200).send({
+                    isSuccess: false,
+                    isUnique: true
+                });
+            } else {
+                res.status(201).send({
+                    isSuccess: true,
+                    isUnique: true,
+                    shortUrl: shortUrl
+                });
+            }
         }
     }
 
+});
+
+router.post("/getLongUrl", async (req, res, next) => {
+    const shortUrl = req.body.shortUrl;
+    var url = await Url.findOne({
+        shortUrl: shortUrl
+    }).select().exec();
+
+    if (!url) {
+        res.status(200).send({
+            isSuccess: false
+        });
+    }
+
+    const timestamp = (new Date()).getTime();
+    const visit = new Visit({
+        shortUrl: shortUrl,
+        timestamp: timestamp
+    })
+    
+    const status = await visit.save();
+    if (!status) {
+        res.status(200).send({
+            isSuccess: false
+        });
+    }
+    res.status(201).send({
+        isSuccess: true,
+        url: url
+    });
+});
+
+router.post("/getStats", async (req, res, next) => {
+    const shortUrl = req.body.shortUrl;
+    var url = await Url.findOne({
+        shortUrl: shortUrl
+    }).select().exec();
+
+    if (!url) {
+        res.status(200).send({
+            isSuccess: false
+        });
+    }
+
+    const visit = await Visit.find({
+        shortUrl: shortUrl
+    }).select("-v -_id").exec();
+
+    res.status(200).send({
+        isSuccess: true,
+        longUrl: url.longUrl,
+        visit: visit
+    });
 });
 
 module.exports = router;
